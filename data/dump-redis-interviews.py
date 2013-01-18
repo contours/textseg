2ac72879206ff91cd550a78ca0496ec6ad124748
Greg Maslov <gmaslov@bootis.org>
2013-01-18 16:44:15 -0500

    Fix up some pesky Unicode smart quotes.

--- data/dump-redis-interviews.py
+++ data/dump-redis-interviews.py
@@ -13,7 +13,19 @@ for interview_id in interview_ids:
     fp = file(interview_id.lstrip('interviews:') + '.txt', 'w')
     sentence_ids = db.lrange(interview_id+':sentences',0,-1)
     for sentence_id in sentence_ids:
-        text = db.hget(sentence_id, 'text')
-        fp.write(text)
+        text = db.hget(sentence_id, 'text').decode('utf8')
+        text = text.translate({
+            0x2013: ord('-'), # en dash
+            0x2014: ord('-'), # em dash
+            0x2018: ord("'"), # left single quote
+            0x2019: ord("'"), # right single quote
+            0x2032: ord("'"), # prime
+            0x2033: ord('"'), # double prime
+            0x201c: ord('"'), # left double quote
+            0x201d: ord('"'), # right double quote
+            0x2026: u"...",   # ellipsis
+            0x07de: ord('-')  # a very strange character which must be an error
+            })
+        fp.write(text.encode('utf8'))
         fp.write("\n")
 
