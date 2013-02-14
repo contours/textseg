045467c3a6bde7dd42f70ec48919ee8c3f123889
Greg Maslov <gmaslov@bootis.org>
2013-02-14 17:42:13 -0500

    add command-line options to dp-experiment

--- dp-experiment.hs
+++ dp-experiment.hs
@@ -13,13 +13,16 @@ import Datasets (load_ds)
 
 main = do
     args <- getArgs
-    when (length args /= 1) $
-        fail "Usage: topictiling-experiment <path to LDA model>"
+    when (length args < 1) $
+        fail "Usage: dp-experiment <path to LDA model> [interview name...]"
     lda <- let lda_file = head args
            in doesFileExist lda_file >>= \case
                True -> decodeFile lda_file
                False -> fail $ "Model file does not exist: "++lda_file
-    testSet <- load_ds
+    dataset <- load_ds
+    let interviewNames = drop 1 args
+    let testSet = if null interviewNames then dataset else
+            filter (\d -> name d `elem` interviewNames) dataset
 
     let config = DP.Config { DP.misraModification = False }
     printf "\"document\",\"sentence gap activity (baseline)\",\"sentence gap activity (unmodified lda)\"\n"
